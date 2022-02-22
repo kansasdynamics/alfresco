1ddb18a1b5d61b866f5fa2a9760f88a9298f61e2
Alex <89443340+kansasdynamics@users.noreply.github.com>
2022-02-22 01:12:40 -0600

    Create install_activemq.sh

---
+++ install_activemq.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Access ActiveMQ console
+# http://<hostname>:8161/admin
+# Update username and password
+# /opt/activemq/conf/jetty-realm.properties
+
+sudo yum install epel-release -y
+sudo yum update -y
+sudo yum install java-11-openjdk -y
+echo "JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")" | sudo tee -a /etc/profile
+wget https://archive.apache.org/dist/activemq/5.16.0/apache-activemq-5.16.0-bin.tar.gz
+sudo tar -zxvf apache-activemq-5.16.0-bin.tar.gz -C /opt
+sudo ln -s /opt/apache-activemq-5.16.0 /opt/activemq
+cd /opt/activemq/bin
+sudo ./activemq start
+sudo systemctl enable activemq.service
+sudo systemctl start activemq.service
+sudo systemctl stop activemq.service
+sudo firewall-cmd --zone=public --permanent --add-port=8161/tcp
+sudo firewall-cmd --reload
+cd ~
