48afcc5dd819668bf32e549f6733adeccac4fe1f
Alex <89443340+kansasdynamics@users.noreply.github.com>
2022-02-22 01:37:40 -0600

    Update install_activemq.sh

--- install_activemq.sh
+++ install_activemq.sh
@@ -13,7 +13,6 @@ sudo tar -zxvf apache-activemq-5.16.0-bin.tar.gz -C /opt
 sudo ln -s /opt/apache-activemq-5.16.0 /opt/activemq
 cd /opt/activemq
 sudo ./bin/activemq start
-sudo systemctl start activemq.service
 sudo firewall-cmd --zone=public --permanent --add-port=8161/tcp
 sudo firewall-cmd --reload
 cd ~
