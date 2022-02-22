2dcfac98d42f299f01ac95854b82e4acace25e19
Alex <89443340+kansasdynamics@users.noreply.github.com>
2022-02-22 01:45:00 -0600

    Update install_activemq.sh

--- install_activemq.sh
+++ install_activemq.sh
@@ -8,13 +8,13 @@ sudo yum install epel-release -y
 sudo yum update -y
 sudo yum install java-11-openjdk -y
 echo "JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")" | sudo tee -a /etc/profile
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+sudo firewall-cmd --zone=public --permanent --add-port=8161/tcp
+sudo firewall-cmd --reload
 wget https://archive.apache.org/dist/activemq/5.16.0/apache-activemq-5.16.0-bin.tar.gz
 sudo tar -zxvf apache-activemq-5.16.0-bin.tar.gz -C /opt
 sudo ln -s /opt/apache-activemq-5.16.0 /opt/activemq
 cd /opt/activemq
 sudo ./bin/activemq start
-sudo systemctl start firewalld
-sudo systemctl enable firewalld
-sudo firewall-cmd --zone=public --permanent --add-port=8161/tcp
-sudo firewall-cmd --reload
 cd ~
